8f9e839761a7c25022a9551039f74b18252f2ae0
vijayranka <letsbejain@gmail.com>
2021-04-20 20:41:20 +0530

    user attached

--- index.js
+++ index.js
@@ -7,16 +7,24 @@ app.get('/', (req, res) => {
     res.send("Node Server is running. Yay!!")
 })
 
+var users = [];
+
 io.on('connection', socket => {
     //Get the chatID of the user and join in a room of the same chatID
     chatID = socket.handshake.query.chatID
     socket.join(chatID)
     console.log("made with socket Id " + chatID);
+    if(!users.includes(chatID))
+    users.push(chatID);
 
     //Leave the room if the user closes the socket
     socket.on('disconnect', () => {
         console.log('user ' + chatID + ' disconnected');
-        socket.leave(chatID)
+        socket.leave(chatID);
+        const index = array.indexOf(chatID);
+            if (index > -1) {
+              users.splice(index, 1);
+            }
     })
 
     //Send message to only a particular user
